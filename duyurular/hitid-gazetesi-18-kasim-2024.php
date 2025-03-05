6d3b1e7110b7cf6e9d23da3364c3d46ce55be632
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-03-05 15:59:12 +0300

    iki yeni duyuru eklendi.

---
+++ duyurular/hitid-gazetesi-18-kasim-2024.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="TR">
+
+<head>
+    <title>hitid-gazetesi-18-kasim-2024 | HİTİD (HLA İmmünogenetiği ve Transplantasyon İmmünolojisi Derneği)</title>
+    <?php include('../head.php') ?>
+
+</head>
+
+<body>
+    <main>
+        <?php include('../header.php') ?>
+
+        <section>
+            <div class="w-100 pt-50 black-layer pb-50 position-relative">
+                <div class="fixed-bg" style="background-color:#246795;"></div>
+                <div class="container">
+                    <div class="page-title-wrap text-center w-100">
+                        <div class="page-title-inner d-inline-block">
+                            <h1 class="mb-0">Duyurular</h1>
+                            <ol class="breadcrumb mb-0 justify-content-center">
+                                    <li class="breadcrumb-item"><a href="/index.php" title="">Anasayfa</a></li>
+                                    <li class="breadcrumb-item"><a href="/duyurular.php" title="">Duyurular</a></li>
+                                </ol>
+                        </div>
+                    </div><!-- Page Title Wrap -->
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="w-100 pt-90 pb-90 position-relative">
+                <div class="container">
+                    <div class="page-wrap position-relative w-100">
+                        <div class="row mrg30 justify-content-center">
+                          
+                             <div class="col-md-12 col-sm-12 col-lg-8  order-mobile-first">
+                                <div class="post-detail w-100">
+                                    <h3>HİTİD Gazetesi 1. Sayı (18 KASIM 2024)</h3>
+                                    <div class="d-flex justify-content-center mt-5">
+                                        <a class="thm-btn scndry-bg brd-rd5 d-inline-block position-relative overflow-hidden text-white" href="/doc/duyurular/hitid-gazetesi.pdf" target="_blank" title="">İndirmek için tıklayın</a></p>
+
+                                    </div>
+                                    <div class="post-feat-img serv-detail-img brd-rd10 position-relative overflow-hidden w-100">
+                                        <img class="img-fluid w-100 mt-5" src="/doc/duyurular/hitid-gazetesi-1.png" alt="Service Detail Image">
+                                        <img class="img-fluid w-100 mt-5" src="/doc/duyurular/hitid-gazetesi-2.png" alt="Service Detail Image">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- Page Wrap -->
+                </div>
+            </div>
+        </section>
+
+        <?php include('../footer.php') ?>
+
+    </main><!-- Main Wrapper -->
+
+    <?php include('../script.php') ?>
+
+</body>
+
+</html>
